6cf20947401228c6dd454882e7163cb4d2693354
Vincent <vincentyrlee@gmail.com>
2025-07-30 12:37:34 +0100

    more structure

---
+++ SUI.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+
+
+
+A= [""]
+
+
+st.set_page_config(
+    page_title="Flight Planner",
+    page_icon="✈️",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+st.title("Flight Planner with Web Scraper")
+st.sidebar.
